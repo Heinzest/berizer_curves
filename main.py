281b59f7ccaf37e4c1a1f4cd684b472a0549241d
Heinzest <jakub.spylczyn@gmail.com>
2021-10-02 22:19:06 +0200

    added way of moving points using mouse, fixed distance bug

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ screen = pygame.display.set_mode((1080, 720))
 show = True
 mousePos = (0, 0)
 i = 0
-intervals = 40
 
 circleTypes = [
     ((255, 0, 0), 0),
@@ -18,48 +17,41 @@ circleTypes = [
 
 
 def distance(point1, point2):
-    return ((point1.x - point2.y) ** 2 + (point1.x - point2.y) ** 2) ** 0.5
+    return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
 
 
 pointList = [
     bc.Point(100, 500),
     bc.Point(500, 100),
-    bc.Point(500, 500),
     bc.Point(900, 500),
+    bc.Point(500, 500),
 ]
 
 cubic_bezier = bc.CubicBezier(pointList[0], pointList[1], pointList[2], pointList[3])
+chosen_point = -1
 
 while show:
 
     for events in pygame.event.get():
-        # if events.type == pygame.MOUSEMOTION:
-        #     mousePos = pygame.mouse.get_pos()
-        #     smallest_dist, smallest_dist_index = 20, -1
-        #     for x in range(len(pointList)):
-        #         if pointList[x].kind == 2:
-        #             pointList[x].kind = 0
-        #         if pointList[x].kind == 0:
-        #             temp_dist = distance(mousePos, pointList[x].coord)
-        #             if temp_dist <= smallest_dist:
-        #                 smallest_dist = temp_dist
-        #                 smallest_dist_index = x
-        #     if smallest_dist <= pointList[smallest_dist_index].size:
-        #         pointList[smallest_dist_index].kind = 2
+        if events.type == pygame.MOUSEMOTION:
+            if chosen_point != -1:
+                mousePos = pygame.mouse.get_pos()
+                pointList[chosen_point].set_cord(*mousePos)
         if events.type == pygame.QUIT:
             sys.exit()
+        if events.type == pygame.MOUSEBUTTONDOWN:
+            mousePos = pygame.mouse.get_pos()
+            for x in range(len(pointList)):
+                dist = distance(mousePos, pointList[x].get_cord())
+                if dist <= 10:
+                    chosen_point = x
+        if events.type == pygame.MOUSEBUTTONUP:
+            chosen_point = -1
 
-        #if events.type == pygame.MOUSEBUTTONDOWN:
-            #mousePos = pygame.mouse.get_pos()
-
-
-    pointList[0].show(screen)
     cubic_bezier.show(screen, True)
 
     i += 1
-
-    pygame.time.wait(150)
-
+    pygame.time.wait(40)
     pygame.display.flip()
     screen.fill((0, 0, 0))
 
